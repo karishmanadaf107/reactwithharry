d034bc4f322d8bb14381d6bb3af3e9ee59477532
karishmanadaf107 <139882105+karishmanadaf107@users.noreply.github.com>
2023-08-24 13:58:50 +0530

    Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -14,12 +14,6 @@ export default function Navbar(props){
    
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-{/*         <li className="nav-item">
-          <a className="nav-link " aria-current="page" href="#">Home</a>  
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">{props.aboutText}</a> 
-        </li> */}
 
         <div className='d-flex'>
 
